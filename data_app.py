92e58491da102b6064237b3f0503eacc9897b5b2
neldrs <nelly.derosan@gmail.com>
2023-11-03 23:54:47 -0700

    Update bucket access paths, add user match data upload, remove match percentage calculation

--- data_app.py
+++ data_app.py
@@ -1,4 +1,4 @@
-from tools.bucket import download_from_bucket, download_all_from_bucket
+from tools.bucket import download_from_bucket, download_all_from_bucket, upload_to_bucket
 from google.cloud import storage
 import pickle
 import re
@@ -13,7 +13,7 @@ def process_blob(blob_path):
     return [tuple(map(float, match)) for match in matches]
 
 destination_folder = "blobs_tmp"
-blob_paths = download_all_from_bucket('brainbit_bucket', destination_folder)
+blob_paths = download_all_from_bucket('brainbit_bucket', destination_folder, prefix="bw_scans/")
 
 client = storage.Client.from_service_account_json('tools/halogen-inkwell-401500-65e54374e3c7.json')
 
@@ -21,41 +21,33 @@ bucket = client.bucket('brainbit_bucket')
 
 blobs = list(bucket.list_blobs())
 
-reference_blob_path = os.path.join(destination_folder, 'bwdata_test1.pkl')
+reference_blob_path = os.path.join(destination_folder, 'bw_scans', 'bwdata_test1.pkl')
 reference_data = process_blob(reference_blob_path)
-
-all_distances = []
+reference_base_name = os.path.splitext(os.path.basename(reference_blob_path))[0]
+matches_pkl_name = f"matches_for_{reference_base_name}.pkl"
+results = {}
+#all_distances = []
 for blob_path in blob_paths:
     if blob_path == reference_blob_path:
         continue
     current_data = process_blob(blob_path)
     distances = []
-
+    blob_name = os.path.basename(blob_path)
     for ref_set, cur_set in zip(reference_data, current_data):
         distance = math.sqrt(sum([(i-j)**2 for i, j in zip(ref_set, cur_set)]))
         distances.append(distance)
 
-    all_distances.extend(distances)
+    #all_distances.extend(distances)
     average_distance = sum(distances) / len(distances)
-    print(f"Average distance for {blob_path}: {average_distance}")
-
-min_distance = min(all_distances)
-max_distance = max(all_distances)
-
-for blob_path in blob_paths:
-    if blob_path == reference_blob_path:
-        continue
-
-    current_data = process_blob(blob_path)
-    distances = [math.sqrt(sum([(i - j) ** 2 for i, j in zip(ref_set, cur_set)])) for ref_set, cur_set in
-                 zip(reference_data, current_data)]
-    match_percentages = [100 * (1 - (distance - min_distance) / (max_distance - min_distance)) for distance in distances]
-    #for idx, match_percentage in enumerate(match_percentages, 1):
-    #    print(f"Set {idx} for blob {blob_path}: {match_percentage}% match")
-
-    average_match_percentage = sum(match_percentages) / len(match_percentages)
-    print(f"Average Match Percentage for {blob_path}: {average_match_percentage}% match")
-
+    results[blob_name] = average_distance
+    print(f"Average distance for {blob_name}: {average_distance}")
     os.remove(blob_path)
-os.remove(reference_blob_path)
 
+with open(matches_pkl_name, 'wb') as pickle_file:
+    pickle.dump(results, pickle_file)
+
+
+upload_to_bucket('brainbit_bucket', matches_pkl_name, f"users_match_data/{matches_pkl_name}")
+
+os.remove(reference_blob_path)
+os.remove(matches_pkl_name)
