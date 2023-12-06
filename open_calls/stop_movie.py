2a3c07193e29c6fb8d71f81ddd49d71f9415f67a
neldrs <nelly.derosan@gmail.com>
2023-12-06 14:58:58 -0800

    Update stop_movie.py

--- open_calls/stop_movie.py
+++ open_calls/stop_movie.py
@@ -8,21 +8,21 @@ import pickle
 
 def handle_request():    
     # Placeholder, demonstrates that the proper user ID was received
-    logger.debug(request.form['userid'])
+    #logger.debug(request.form['userid'])
     userid = request.form['userid'] + "_HBscan.pkl"
-    
+
     # If headband library is not installed or headband is not connected
     if 'hb' not in g or g.hb == None:
         return ["Stop, no HB"]
 
     g.hb.exec_command(SensorCommand.CommandStopSignal)
-    with open('output_pickle.pkl', 'wb') as file:
+    with open('output.pkl', 'wb') as file:  # Changed file name to 'output.pkl'
         pickle.dump(all_my_data, file)
 
     bucket_name = 'brainbit_bucket'
-    source_file_path = r'output.pkl'
+    source_file_path = 'output.pkl'  # Ensure this matches the file you just wrote
     destination_blob_name = userid  # Name it will have in the bucket
-    upload_to_bucket(bucket_name, source_file_path, destination_blob_name)
+    upload_to_bucket(bucket_name, source_file_path, f"bw_scans/{destination_blob_name}")
 
 
 
